b0ae7e9e523b80a4648328175c19c9b7f13775e9
yoonxjoong <yhj8372@naver.com>
2024-08-30 15:40:18 +0900

    feat: Initial Commit

---
+++ bff-homework/src/main/java/org/musinsa/api/BffHomeworkApplication.java
@@ -0,0 +1,13 @@
+package org.musinsa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffHomeworkApplication.class, args);
+	}
+
+}
---
+++ bff-homework/src/test/java/org/musinsa/api/BffHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.musinsa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
